9f7afff32d3cf599b3650bc0cecdaacc371c7389
Bojan Delic <bojan.delic@sbgenomics.com>
2015-11-19 01:27:21 +0100

    Move config and transformers into own files

--- jsonresponse.go
+++ jsonresponse.go
@@ -27,86 +27,9 @@ package jsonresponse
 import (
 	"encoding/json"
 	"net/http"
-	"sync"
 	"fmt"
 	"bytes"
 )
-
-// ResponseTransformer is function that transforms response before it is send
-// to client. Default implementation is provided, but it can suite
-// more specific needs.
-type ResponseTransformer func(resp Response, httpCode int) (headers map[string]string, result interface{})
-
-var (
-	transformerLock = &sync.Mutex{}
-
-	// Transformer for response. Default implementation wraps response in
-	// SBG envelope (with status and message).
-	transformer ResponseTransformer = defaultTransformer
-)
-
-var (
-	contentTypeHeaderLock = &sync.Mutex{}
-
-	// Default Content-Type header for Json responses.
-	defaultContentTypeHeader string = "application/json; charset=utf-8"
-)
-
-var (
-	indentLock = &sync.Mutex{}
-
-	// indent sets flag that indicates that all json responses will be
-	// indented before returned. This can be useful for debugging when
-	// consumer of JSON response is developer and not other service.
-	indent bool = false
-)
-
-// SetTransformer sets function that will process response additionally.
-// Default implementation just wraps response value in map with key "data".
-func SetTransformer(t ResponseTransformer) {
-	transformerLock.Lock()
-	defer transformerLock.Unlock()
-	transformer = t
-}
-
-// ResetTransformer resets current transformer to default one.
-func ResetTransformer() {
-	transformerLock.Lock()
-	defer transformerLock.Unlock()
-	transformer = defaultTransformer
-}
-
-// SetDefaultContentTypeHeader sets string that will be included in header
-// under Content-Type header. This will only be included if transformer function
-// does not already set Content-Type header.
-func SetDefaultContentTypeHeader(contentType string) {
-	contentTypeHeaderLock.Lock()
-	defer contentTypeHeaderLock.Unlock()
-	defaultContentTypeHeader = contentType
-}
-
-// SetIndent sets flat that indicates that JSON response messages should
-// be indented before sending them to client. This can be useful for
-// debugging when consumer of JSON response is developer and not
-// other service.
-func SetIndent(flag bool) {
-	indentLock.Lock()
-	defer indentLock.Unlock()
-	indent = flag
-}
-
-
-// defaultTransformer just wraps response dict with key data.
-func defaultTransformer(resp Response, httpCode int) (headers map[string]string, result interface{}) {
-	h := map[string]string{}
-	r := map[string]interface{}{
-		"data": resp.Data,
-	}
-	if resp.Excuse != "" {
-		r["programming-excuse"] = resp.Excuse
-	}
-	return h, r
-}
 
 // Response object, only contains object to return.
 type Response struct {
---
+++ transformer.go
@@ -0,0 +1,50 @@
+package jsonresponse
+
+// ResponseTransformer is function that transforms response before it is send
+// to client. Default implementation is provided, but it can suite
+// more specific needs.
+type ResponseTransformer func(resp Response, httpCode int) (headers map[string]string, result interface{})
+
+// PassthroughTransformer only returns data as they are in response without modification.
+func PassthroughTransformer(resp Response, httpCode int) (headers map[string]string, result interface{}) {
+	return make(map[string]string), resp.Data
+}
+
+func MessageCodeTransformer(dataField string, codeField string) ResponseTransformer {
+	return ResponseTransformer(func (resp Response, httpCode int) (headers map[string]string, result interface{}) {
+		h := map[string]string{}
+		r := map[string]interface{}{
+			dataField: resp.Data,
+			codeField: httpCode,
+		}
+		return h, r
+	})
+}
+
+
+func MessageCodeExcuseTransformer(dataField string, codeField string) ResponseTransformer {
+	return ResponseTransformer(func (resp Response, httpCode int) (headers map[string]string, result interface{}) {
+		h := map[string]string{}
+		r := map[string]interface{}{
+			dataField: resp.Data,
+			codeField: httpCode,
+		}
+		if resp.Excuse != "" {
+			r["programming-excuse"] = resp.Excuse
+		}
+		return h, r
+	})
+}
+
+
+// defaultTransformer just wraps response dict with key data.
+func defaultTransformer(resp Response, httpCode int) (headers map[string]string, result interface{}) {
+	h := map[string]string{}
+	r := map[string]interface{}{
+		"data": resp.Data,
+	}
+	if resp.Excuse != "" {
+		r["programming-excuse"] = resp.Excuse
+	}
+	return h, r
+}
